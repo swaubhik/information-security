64444f6aa7fdb28df20e8e6b58ca01b5f73ebec4
swaubhik <b18csel299@cit.ac.in>
2021-06-07 21:26:59 +0530

    👌 IMPROVE: Javascript

---
+++ assets/scripts/script.js
@@ -0,0 +1,77 @@
+//To get index of the encryption or decryption algotithm
+function getEncryptIndex() {
+  let index = document.getElementById("encyrptChoice").selectedIndex;
+  return index;
+}
+function getDecryptIndex() {
+  let index = document.getElementById("decryptChoice").selectedIndex;
+  return index;
+}
+
+//To reset form elements
+function resetForm1() {
+  document.getElementById("form1").reset();
+}
+function resetForm2() {
+  document.getElementById("form2").reset();
+}
+
+//To get text from input cell
+function getEncryptInput() {
+  let text = document.getElementById("encryptPlainText").value;
+  return text;
+}
+function getDecryptInput() {
+  let text = document.getElementById("decryptCipherText").value;
+  return text;
+}
+
+//To get key value
+function getEncryptKey() {
+  let key = document.getElementById("encryptKey").value;
+  return key;
+}
+function getDecryptKey() {
+  let key = document.getElementById("decryptKey").value;
+  return key;
+}
+
+//To write text to output cell
+function setEncryptOutput(output) {
+  document.getElementById("encryptCipherText").value = output;
+}
+function setDecryptOutput(output) {
+  document.getElementById("decryptPlainText").value = output;
+}
+
+//ENCRYPT FUNCTION
+function encrypt() {
+  let text = getEncryptInput();
+  let choice = getEncryptIndex();
+
+  if (choice == 0) {
+    let key = parseInt(getEncryptKey());
+    if (isNaN(key) || key < 1 || key > 25) {
+      alert("The Key Value should be 1 - 25");
+      return false;
+    }
+    let cipherText = caeserCipherEncrypt(text, key);
+    setEncryptOutput(cipherText);
+  }
+}
+
+//DECRYPT FUNCTION
+function decrypt() {
+  let text = getDecryptInput();
+  let choice = getDecryptIndex();
+
+  if (choice == 0) {
+    let key = parseInt(getDecryptKey());
+    if (isNaN(key) || key < 1 || key > 25) {
+      alert("The Key Value should be 1 - 25");
+      return false;
+    }
+    let plainText = caeserCipherDecrypt(text, key);
+    setDecryptOutput(plainText);
+  }
+}
