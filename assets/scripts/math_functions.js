8d398e40d2b5cf647cebd57791096e74e034d898
swaubhik <b18csel299@cit.ac.in>
2021-06-12 19:03:33 +0530

    📦 NEW: matrix multiplication

--- assets/scripts/math_functions.js
+++ assets/scripts/math_functions.js
@@ -1,11 +1,42 @@
-//Returns A % B (Supports negative number as well)
-function modulo(a, b){
-    if (b < 1)
-    {
-        console.log("The second value should be greater than 0");
+//*Returns A % B (Supports negative number as well)
+function modulo(a, b) {
+  if (b < 1) {
+    console.log("The second value should be greater than 0");
+  }
+  let mod = 0;
+  let c = Math.floor(a / b);
+  mod = a - b * c;
+  return mod;
+}
+//*Returns a * b matrices
+
+function multiplyMatrices(a, b) {
+  if (!Array.isArray(a) || !Array.isArray(b) || !a.length || !b.length) {
+    throw new Error("Should be in 2-D array format");
+  }
+  let x = a.length,
+    z = a[0].length,
+    y = b[0].length;
+  if (b.length !== z) {
+    // XxZ & ZxY => XxY
+    throw new Error(
+      "number of columns in the first matrix should be the same as the number of rows in the second"
+    );
+  }
+  let productRow = Array.apply(null, new Array(y)).map(
+    Number.prototype.valueOf,
+    0
+  );
+  let product = new Array(x);
+  for (let p = 0; p < x; p++) {
+    product[p] = productRow.slice();
+  }
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < y; j++) {
+      for (let k = 0; k < z; k++) {
+        product[i][j] += a[i][k] * b[k][j];
+      }
     }
-    let mod = 0;
-    let c = Math.floor(a / b);
-    mod = a - (b * c);
-    return mod;
+  }
+  return product;
 }
