827de025f61a2a8bd7ed3cbb586d608ecf8b4e1c
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2021-06-12 20:27:37 +0530

    added determinant functions

--- assets/scripts/math_functions.js
+++ assets/scripts/math_functions.js
@@ -1,9 +1,8 @@
 //Returns A % B (Supports negative number as well)
-function modulo(a, b){
-  if (b < 1)
-  {
-      console.log("The second value should be greater than 0");
-      return false;
+function modulo(a, b) {
+  if (b < 1) {
+    console.log("The second value should be greater than 0");
+    return false;
   }
   let mod = 0;
   let c = Math.floor(a / b);
@@ -13,75 +12,111 @@ function modulo(a, b){
 
 //*Returns a * b matrices
 
-function multiplyMatrices(a, b) {
-  if (!Array.isArray(a) || !Array.isArray(b) || !a.length || !b.length) {
-    throw new Error("Should be in 2-D array format");
-  }
-  let x = a.length,
-    z = a[0].length,
-    y = b[0].length;
-  if (b.length !== z) {
+function multiplyMatrices(mat1, mat2, m1, n1, m2, n2) {
+  if (n1 != m2) {
     // XxZ & ZxY => XxY
-    throw new Error(
-      "number of columns in the first matrix should be the same as the number of rows in the second"
-    );
+    console.log("Row != Column");
   }
-  let productRow = Array.apply(null, new Array(y)).map(
-    Number.prototype.valueOf,
-    0
-  );
-  let product = new Array(x);
-  for (let p = 0; p < x; p++) {
-    product[p] = productRow.slice();
-  }
-  for (let i = 0; i < x; i++) {
-    for (let j = 0; j < y; j++) {
-      for (let k = 0; k < z; k++) {
-        product[i][j] += a[i][k] * b[k][j];
+  let x, i, j;
+  let result = new Array(m1);
+  for (i = 0; i < m1; i++)
+    result[i] = new Array(n2);
+  for (i = 0; i < m1; i++) {
+    for (j = 0; j < n2; j++) {
+      result[i][j] = 0;
+      for (x = 0; x < m2; x++) {
+        result[i][j] += mat1[i][x] * mat2[x][j];
       }
     }
   }
-  return product;
+  return result;
 }
 
 //*Return Multiplicative Inverse Modulo value
-function multInverse(a, b){
-  if (a < 0)
-  {
-      a = modulo(a, b);
+function multInverse(a, b) {
+  if (a < 0) {
+    a = modulo(a, b);
   }
-  if (b < 0){
-      console.log("The second value should be greater than 0");
-      return false;
+  if (b < 0) {
+    console.log("The second value should be greater than 0");
+    return false;
   }
   var a1 = 1; var b1 = 0;
   var a2 = 0; var b2 = 1;
   var a3 = b; var b3 = a;
   let loop = true;
-  while(loop)
-  {
-      if (b3 == 0)
-      {
-          loop = false;
-          console.log("No inverse is available.");
-          return false;
-      }
-      if (b3 == 1)
-      {
-          loop = false;
-          let num = modulo(b2, b);
-          return num;
-      }
-      let c = Math.floor(a3 / b3);
-      var t1 = a1 - (b1 * c);
-      var t2 = a2 - (b2 *c );
-      var t3 = a3 - (b3 * c);
-      a1 = b1;
-      a2 = b2;
-      a3 = b3;
-      b1 = t1;
-      b2 = t2;
-      b3 = t3;
-      loop = true;
-  }  
+  while (loop) {
+    if (b3 == 0) {
+      loop = false;
+      console.log("No inverse is available.");
+      return false;
+    }
+    if (b3 == 1) {
+      loop = false;
+      let num = modulo(b2, b);
+      return num;
+    }
+    let c = Math.floor(a3 / b3);
+    var t1 = a1 - (b1 * c);
+    var t2 = a2 - (b2 * c);
+    var t3 = a3 - (b3 * c);
+    a1 = b1;
+    a2 = b2;
+    a3 = b3;
+    b1 = t1;
+    b2 = t2;
+    b3 = t3;
+    loop = true;
+  }
 }
+
+
+//* CoFactor
+function getCofactor(mat, temp, p, q, n) {
+  let i = 0, j = 0;
+  for (let row = 0; row < n; row++) {
+    for (let col = 0; col < n; col++) {
+      if (row != p && col != q) {
+        temp[i][j++] = mat[row][col];
+        if (j == n - 1) {
+          j = 0;
+          i++;
+        }
+      }
+    }
+  }
+}
+
+//* Determinant
+function determinantOfMatrix(mat, n) {
+  let D = 0;
+  if (n == 1)
+    return mat[0][0];
+  let temp = new Array(n);
+  for (let i = 0; i < n; i++) {
+    temp[i] = new Array(n);
+  }
+  let sign = 1;
+  for (let f = 0; f < n; f++) {
+    getCofactor(mat, temp, 0, f, n);
+    D += sign * mat[0][f] * determinantOfMatrix(temp, n - 1);
+    sign = -sign;
+  }
+  return D;
+}
+
+// //! Do not delete
+// let mat = [
+//   [6, 24, 1],
+//   [13, 16, 10],
+//   [20, 17, 15]
+// ];
+// let n = 3
+// let temp = new Array(n);
+// for (let i = 0; i < n; i++) {
+//   temp[i] = new Array(n);
+// }
+// //! Do not delete
+// getCofactor(mat, temp, 0, 0, n);
+// console.log(determinantOfMatrix(temp, n - 1));
+
