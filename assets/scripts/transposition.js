6518b080c8572a6f7ad7e3a3a7f4fa5f4d9be9d5
swaubhik <b18csel299@cit.ac.in>
2021-07-15 21:22:23 +0530

    :rocket: Trasnposition

--- assets/scripts/transposition.js
+++ assets/scripts/transposition.js
@@ -1,60 +1,84 @@
-function Create2DArray(rows) {
-    var arr = [];
-    for (var i=0;i<rows;i++) {
-       arr[i] = [];
+let input = "kon motherchod humra maal ko phaswala";
+let encodedStr = "";
+let key = 52314;
+function encode(str) {
+  let finalStr = "";
+  let manipulationArr = [];
+  let lineArr = [];
+  let keyStr = key.toString();
+
+  let currStr = keyStr + str.replace(/\s/g, " ");
+  let totalLines = Math.round(currStr.length / keyStr.length);
+
+  for (let i = 1; i <= currStr.length; i++) {
+    lineArr.push(currStr[i - 1]);
+    if (i % keyStr.length === 0 && i > 0) {
+      manipulationArr.push(lineArr);
+      lineArr = [];
     }
-    return arr;
   }
-  
-  function transKeyCalc(key){
-    var temp = "";
-    for(var i=0;i<key.length;i++){
-      if(temp.indexOf(key[i]) < 0){
-        temp = temp + "" + key[i];
+
+  if (manipulationArr.length - 1 < totalLines) manipulationArr.push(lineArr);
+
+  let aux = "";
+  for (let i = 0; i < manipulationArr.length; i++) {
+    for (let j = 0; j < manipulationArr[0].length - 1; j++) {
+      if (manipulationArr[0][j + 1] < manipulationArr[0][j]) {
+        for (let h = 0; h < manipulationArr.length; h++) {
+          aux = manipulationArr[h][j];
+          manipulationArr[h][j] = manipulationArr[h][j + 1];
+          manipulationArr[h][j + 1] = aux;
+        }
       }
     }
-    var ascii = [];
-    for (i = 0; i < temp.length; i++) {
-      ascii.push(temp.charCodeAt(i));
-    }
-    var ascii2 = _.map(ascii, _.clone);
-    ascii2.sort(function(a, b){return a-b});
-    for(i=0; i<ascii2.length; i++){
-      ascii[ascii.indexOf(ascii2[i])] = i;
-    }
-    return ascii;
-  } 
-  
-  function encrypt(){
-    var key = document.getElementById('key').value;
-    document.getElementById('output').value = '';
-    var op = '';
-    var ip = document.getElementById('input').value;
-    var encrKey = transKeyCalc(key);
-    var m = encrKey.length;
-    var n = Math.ceil( ip.length / m );
-    var totalLetters = n*m;
-    var noOfHashes = totalLetters - ip.length;
-    var hashes = '';
-    for(var i=0;i<noOfHashes;i++){
-      hashes = hashes + "#";
-    }
-    var plaintext = ip + hashes;
-    var arr = Create2DArray(n);
-    for(var i=0;i<n;i++){
-      for(var j=0;j<m;j++){
-        arr[i].push(plaintext[(i*m)+j]);
-      }
-    }
-    var temp=0;
-    for(var j=0;j<m;j++){
-      temp = encrKey[j];
-      for(var i=0;i<n;i++){
-        op = op + "" + arr[i][temp];
-      }
-      op = op + " ";
-    } 
-    document.getElementById('output').value = op;
-    
   }
-  encrypt(key); 
+
+  manipulationArr.shift(manipulationArr[0]);
+
+  aux = 0;
+  for (let line of manipulationArr) {
+    if (typeof manipulationArr[0][0] == "undefined") return "";
+    for (let char of line) {
+      if (!char) char = " ";
+      finalStr += char;
+    }
+    aux++;
+  }
+
+  return finalStr;
+}
+
+function decode(str) {
+  let finalStr = "";
+  let currStr = str.replace(/;/g, " ");
+  let manipulationArr = [];
+  let keyStr = key.toString();
+
+  let totalLines = Math.round(currStr.length / keyStr.length);
+
+  for (let i = 0; i < totalLines; i++) manipulationArr.push([]);
+
+  if (manipulationArr.length * keyStr.length < currStr.length) manipulationArr.push([]);
+
+  let aux = 0;
+  let index = 0;
+  for (let char of currStr) {
+    manipulationArr[index][aux] = char;
+    if (aux < key.toString().length - 1) aux++;
+    else {
+      index++;
+      aux = 0;
+    }
+  }
+
+  for (let line of manipulationArr) {
+    for (let i = 0; i < keyStr.length; i++) {
+      finalStr += line[keyStr[i] - 1];
+    }
+  }
+
+  return finalStr.replace(/~/g, "").replace(/undefined/g, "");
+}
+
+console.log(encode(input));
+console.log(decode(encode(input)));
